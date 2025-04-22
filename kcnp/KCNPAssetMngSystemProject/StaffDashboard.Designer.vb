329c6922f31c1c8d0579442f089f7d783f647a34
jsonwamuyu <jysonmuchiri@gmail.com>
2025-04-22 20:17:37 +0300

    start staff dashboard layout

--- kcnp/KCNPAssetMngSystemProject/StaffDashboard.Designer.vb
+++ kcnp/KCNPAssetMngSystemProject/StaffDashboard.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class StaffDashboard
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,18 +20,62 @@ Partial Class StaffDashboard
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(StaffDashboard))
+        Me.lblWelcomeStaff = New System.Windows.Forms.Label()
+        Me.btnLogoutStaff = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'lblWelcomeStaff
+        '
+        Me.lblWelcomeStaff.AutoSize = True
+        Me.lblWelcomeStaff.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWelcomeStaff.ForeColor = System.Drawing.Color.Green
+        Me.lblWelcomeStaff.Location = New System.Drawing.Point(63, 17)
+        Me.lblWelcomeStaff.Name = "lblWelcomeStaff"
+        Me.lblWelcomeStaff.Size = New System.Drawing.Size(148, 15)
+        Me.lblWelcomeStaff.TabIndex = 0
+        Me.lblWelcomeStaff.Text = "Welcome, [Username]"
+        '
+        'btnLogoutStaff
+        '
+        Me.btnLogoutStaff.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogoutStaff.ForeColor = System.Drawing.Color.BlueViolet
+        Me.btnLogoutStaff.Location = New System.Drawing.Point(608, 11)
+        Me.btnLogoutStaff.Name = "btnLogoutStaff"
+        Me.btnLogoutStaff.Size = New System.Drawing.Size(75, 25)
+        Me.btnLogoutStaff.TabIndex = 1
+        Me.btnLogoutStaff.Text = "LOGOUT"
+        Me.btnLogoutStaff.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnLogoutStaff)
+        Me.Panel1.Controls.Add(Me.lblWelcomeStaff)
+        Me.Panel1.Location = New System.Drawing.Point(148, 7)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(768, 392)
+        Me.Panel1.TabIndex = 0
         '
         'StaffDashboard
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(971, 496)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "StaffDashboard"
         Me.Text = "StaffDashboard"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents lblWelcomeStaff As Label
+    Friend WithEvents btnLogoutStaff As Button
+    Friend WithEvents Panel1 As Panel
 End Class
--- kcnp/KCNPAssetMngSystemProject/StaffDashboard.vb
+++ kcnp/KCNPAssetMngSystemProject/StaffDashboard.vb
@@ -1,10 +1,32 @@
-﻿Public Class StaffDashboard
+﻿Imports System.Data.OleDb
+
+Public Class StaffDashboard
+    Dim conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\HomePC\Desktop\KCNPAssetManagementSystem\FinalYearKCNPASSETSMNGSYSTEM\KCNP_Assets.accdb")
+
     Private Sub StaffDashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.StartPosition = FormStartPosition.CenterScreen
+
+
+        ' Prevent unauthorized access
         If Session.LoggedInRole <> "Staff" Then
             MessageBox.Show("Unauthorized access!")
             Me.Close()
-            Dim loginForm As New LoginForm()
-            loginForm.Show()
+            LoginForm.Show()
+            Exit Sub
         End If
+
+        lblWelcomeStaff.Text = "Welcome, " & Session.LoggedInUsername
+
+        'LoadAvailableAssets()
+        'LoadMyRequests()
+    End Sub
+
+    'Logout user and redirect to Login Page
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLogoutStaff.Click
+        Session.LoggedInUsername = ""
+        Session.LoggedInRole = ""
+        Me.Close()
+        Dim login As New LoginForm()
+        login.Show()
     End Sub
 End Class
